e160dcfa9482b6e12e195deaf27d747042b54866
Liz Goff <livingearthcreations@gmail.com>
2018-05-03 11:22:21 -0500

    talking with database get and post up

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const Pool = pg.Pool
+const PORT = 5000
+app.use(bodyParser.json());
+
+
+const pool = new Pool ({
+    database: 'shoe_store',
+    host: 'localHost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('Postman connected');
+
+});
+
+pool.on('error', (error) => {
+    console.log('Error with Postman pool', error);
+});
+
+// app.use(express.static('server/public'));
+
+
+const shoes = [
+    {
+        name: 'sandals',
+        cost: '15'
+    }
+];
+
+app.get('/shoe', (req, res) => {
+    console.log('GET /shoes');
+    res.send(shoes);
+});
+
+app.post('/shoe', (req, res) => {
+    const shoe = req.body;
+    pool.query(`INSERT INTO "shoes" ("name", "cost")
+                VALUES ($1, $2);`, [shoe.name, shoe.cost]) 
+                //the shoe.name and shoe.cost is the sanitization for security
+            .then((results) => {
+                res.sendStatus(200);
+            })
+            .catch((error) => {
+                console.log('Error with Postman pool', error);
+                res.sendStatus(500)
+            });
+    // shoes.push(req.body);
+
+});
+
+app.listen(PORT, function (req, res) {
+    console.log('Listening on port', PORT);
+});
